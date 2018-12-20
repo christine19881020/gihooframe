bcea6e8773913c449cdafb9988f3d1aef821eddd
张丹鸯 <zdy_19881020@163.com>
2018-12-20 15:55:57 +0800

    通讯录弹窗预览

--- src/components/commons/fileupload.vue
+++ src/components/commons/fileupload.vue
@@ -1,7 +1,7 @@
 <template>
 	<section>
-	<div class="filebox clearfix" style="margin-bottom:40px;">
-		<h2>
+		<div class="filebox clearfix" style="margin-bottom:40px;">
+			<h2>
 			<span>文件</span>
 			<el-upload class="upload-demo" :headers="header" :action="actionURL" :on-preview="handlePreview" :on-progress="handleProgress"
 			 :on-remove="handleRemove" :on-success="handleSuccess" list-type="picture" :before-upload="beforeAvatarUpload"
@@ -9,57 +9,75 @@
 				<el-button size="mini">上传文件</el-button>
 			</el-upload>
 		</h2>
-		<ul>
-			<li v-if="progress>0&&progress<100">
-				<div class="filetype">
-					<el-progress style="margin-top:5px;" type="circle" :width=70 :percentage="progress" status="success"></el-progress>
-				</div>
-			</li>
-			<li v-for="(item,index) in defaultList" :key="index">
-				<div class="filetype">
-					<!--<img v-if="item.FileType=='ppt'" src="../../assets/ppt.png" class="type" />
+			<ul>
+				<li v-if="progress>0&&progress<100">
+					<div class="filetype">
+						<el-progress style="margin-top:5px;" type="circle" :width=70 :percentage="progress" status="success"></el-progress>
+					</div>
+				</li>
+				<li v-for="(item,index) in defaultList" :key="index">
+					<div class="filetype">
+						<!--<img v-if="item.FileType=='ppt'" src="../../assets/ppt.png" class="type" />
 					<img v-if="item.FileType=='doc'||item.FileType=='docx'" src="../../assets/doc.png" class="type" />
 					<img v-if="item.FileType=='xls'||item.FileType=='xlsx'" src="../../assets/xls.png" class="type" />
 					<img v-if="item.FileType=='pdf'" src="../../assets/pdf.png" class="type" />-->
-				
-					<img class="pdfSize" style="width:40%;margin-top:7px;" v-if="imgTypes.indexOf(item.FileType)<=-1&&item.FileType!='folder'" :alt="item.FileName" :FileId="item.FileId" :src="'https://tower.im/assets/file_icons/file_extension_'+item.FileType+'.png'" :title="item.FileName" @click="gofileviewFn(item)" />			
-					<img class="otherSize" v-if="!item.FileType" :alt="item.FileName" :FileId="item.FileId" src="../../assets/file_extension_others.png" />
-					<span class="hasnotype" v-if="imgTypes.indexOf(item.FileType)==-1">{{item.FileType}}</span>
-					
-					<img v-if="imgTypes.indexOf(item.FileType)==-1&&item.FileType!='ppt'&&item.FileType!='xls'&&item.FileTyp!='xlsx'&&item.FileType!='pdf'"  src="../../assets/file_extension_others.png" width="78px" height="78px" class="type"/>
-					
-					<template v-if="imgTypes.indexOf(item.FileType)>-1">
-						<!--图片后缀文件-->
-						<img class="pic" :alt="item.FileName" :src="imgurl+item.FilePath" @click="gobigImgFn(item)" />
-					</template>
-					<template v-else-if="item.FileType=='ppt'||
+
+						<img class="pdfSize" style="width:40%;margin-top:7px;" v-if="imgTypes.indexOf(item.FileType)<=-1&&item.FileType!='folder'" :alt="item.FileName" :FileId="item.FileId" :src="'https://tower.im/assets/file_icons/file_extension_'+item.FileType+'.png'" :title="item.FileName" @click="gofileviewFn(item)" />
+						<img class="otherSize" v-if="!item.FileType" :alt="item.FileName" :FileId="item.FileId" src="../../assets/file_extension_others.png" />
+						<span class="hasnotype" v-if="imgTypes.indexOf(item.FileType)==-1">{{item.FileType}}</span>
+
+						<!--<img v-if="imgTypes.indexOf(item.FileType)==-1&&item.FileType!='ppt'&&item.FileType!='xls'&&item.FileTyp!='xlsx'&&item.FileType!='pdf'"  src="../../assets/file_extension_others.png" width="78px" height="78px" class="type"/>-->
+
+						<template v-if="imgTypes.indexOf(item.FileType)>-1">
+							<!--图片后缀文件-->
+							<img class="pic" :alt="item.FileName" :src="imgurl+item.FilePath" @click="gobigImgFn(item)" />
+						</template>
+						<!--<template v-else-if="item.FileType=='ppt'||
 						item.FileType=='doc'||item.FileType=='docx'||
 						item.FileType=='xls'||item.FileType=='xlsx'||
 						item.FileType=='pdf'
 						">
 						<!--有设计ICON文件-->
-						<span class="havetype">
+						<!--<span class="havetype">
 							{{item.FileType}}
 						</span>
 					</template>
-					<template v-else>
+					<template v-else>-->
 						<!--非图片和没有设计格式图片的文件-->
-						<span class="hasnotype" v-if="imgTypes.indexOf(item.FileType)==-1">{{item.FileType}}</span>
-					</template>
-				</div>
-				<div class="link-name">{{FileNameFn(item)}}</div>
-				<div class="file-links">
-					<a href="javascript:;" @click="RemoveFolderOrFileFn(item)">删除</a>
-					<a  href="javascript:;" @click="downloadFn(item)">下载</a>
-				</div>
+						<!--<span class="hasnotype" v-if="imgTypes.indexOf(item.FileType)==-1">{{item.FileType}}</span>
+					</template>-->
+					</div>
+					<div class="link-name">{{FileNameFn(item)}}</div>
+					<div class="file-links">
+						<a href="javascript:;" @click="RemoveFolderOrFileFn(item)">删除</a>
+						<a href="javascript:;" @click="downloadFn(item)">下载</a>
+					</div>
 
-			</li>
-		</ul>
-	</div>
-	
-	<el-dialog :visible.sync="dialogVisible" :append-to-body=true>
+				</li>
+			</ul>
+		</div>
+
+		<el-dialog width="80%" :visible.sync="dialogVisible" :append-to-body=true>
 			<img width="100%" :src="dialogImageUrl" alt="">
-	</el-dialog>
+		</el-dialog>
+
+		<!--<el-dialog width="swidth" height="sheight" :modal=false :title="pretitle" :visible.sync="dialogVisiblePreview" :append-to-body=true>
+		<iframe :src="predlgurl" width="100%" height="100%">
+				
+		</iframe>
+	</el-dialog>-->
+
+		<div ref="predlg" class="predlg" :class="{'zoombig':zoombig}" v-if="predlgshow">
+			<div class="preOprate">
+				<div class="pretitle">
+					{{pretitle}}
+				</div>
+				<i class="iconfont icon-guanbi" @click="predlgcloseFn"></i>
+				<i class="iconfont icon-fangkuang" @click="zoombigFn"></i>
+			</div>
+			<iframe :src="predlgurl" frameborder="0"></iframe>
+		</div>
+
 	</section>
 </template>
 
@@ -76,6 +94,8 @@
 		},
 		data() {
 			return {
+				pretitle: '',
+				zoombig: false,
 				fileUrl: 'https://www.jihuobao.net/filecenter/ResourceFile/UploadifyFile?',
 				imgurl: 'https://www.jihuobao.net/filecenter',
 				header: {
@@ -87,25 +107,38 @@
 				defaultList: [],
 				uploadList: [],
 				progress: 0,
-				dialogVisible:false,
-				dialogImageUrl:''
+				dialogVisible: false,
+				dialogImageUrl: '',
+				predlgshow: false,
+				predlgurl: '',
 			}
 		},
 		methods: {
 			setHead() {
 				let code = Cookies.get('gihoo_v1.1_token');
-				if (code) {
+				if(code) {
 					this.header.Authorization = 'Bearer ' + code;
-				}								
+				}
+			},
+			predlgcloseFn(){
+				this.predlgshow=false;
 			},
 			gobigImgFn(file) {
 				this.dialogVisible = true;
 				this.dialogImageUrl = this.imgurl + file.FilePath;
 			},
+			zoombigFn() {
+				this.zoombig = !this.zoombig;
+			},
 			gofileviewFn(file) {
+				console.log(file)
 				file.filepath = file.FilePath ? file.FilePath : file.response.FilePath;
-//				http://192.168.0.125/gihoo/?api/view&path=https://www.jihuobao.net:11443/Resource/JDG18111435.xls
-				window.open('http://192.168.0.125/gihoo/?api/view&path=https://jihuobao.net/filecenter/' + file.filepath, 'newwindow');
+				this.pretitle = file.FileName;
+				this.predlgurl = 'http://192.168.0.125/gihoo/index.php?pluginApp/to/yzOffice/&path=https://jihuobao.net/filecenter/' + file.filepath;
+				this.predlgshow = true;
+				//				http://192.168.0.125/gihoo/?api/view&path=https://www.jihuobao.net:11443/Resource/JDG18111435.xls
+				//				window.open('http://192.168.0.125/gihoo/?api/view&path=https://jihuobao.net/filecenter/' + file.filepath, 'newwindow');
+				//				window.open('http://192.168.0.125/gihoo/index.php?pluginApp/to/yzOffice/&path=https://jihuobao.net/filecenter/' + file.filepath, 'newwindow');
 			},
 			beforeAvatarUpload(file) {
 				console.log(file)
@@ -150,8 +183,8 @@
 			handleSuccess(response, file, fileList) {
 				file.FileName = file.name;
 				this.$message({
-					type:'success',
-					message:response.errmsg
+					type: 'success',
+					message: response.errmsg
 				});
 				this.progress = 0;
 				this.getfilesFn();
@@ -165,7 +198,7 @@
 				token = sessionStorage.getItem('code');
 				// console.log('token', token);				
 				window.location.href = this.imgurl + "/ResourceFile/downloadfile?keyValue=" + file.FileId +
-						'&token=Bearer ' + token;				
+					'&token=Bearer ' + token;
 			},
 			RemoveFolderOrFileFn(item) {
 				this.$confirm('此操作将删除该文件, 是否继续?', '提示', {
@@ -181,7 +214,7 @@
 							type: 'success',
 							message: res.message
 						});
-						if (res.type == 1) {
+						if(res.type == 1) {
 							this.getfilesFn();
 						}
 					})
@@ -199,7 +232,6 @@
 				this.getfilesFn();
 				this.actionURL = this.fileUrl + 'module=6&keyValue=' + this.userid;
 			}, 300)
-
 
 		}
 	}
